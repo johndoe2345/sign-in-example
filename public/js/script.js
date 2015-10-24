895f6a4f6ef67e2bb1b48cde4d1b581489216976
johndoe <johndoe2345646@vfemail.net>
2015-10-24 16:22:56 -0400

    added styling

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+// $(".flash-notice").slideDown(function() {
+//     setTimeout(function() {
+//         $(".flash-notice").slideUp();
+//     }, 5000);
+// });
+// $(".flash-alert").slideDown(function() {
+//     setTimeout(function() {
+//         $(".flash-alert").slideUp();
+//     }, 5000);
+// });
